7aeecfb50e1b91ab28d460ad4c9648b9cc3295fb
Yazeed Allabadi <allabadiyazeed67@gmail.com>
2024-03-14 18:03:31 +0300

    modified login.py

--- login.py
+++ login.py
@@ -1,11 +1,14 @@
 import customtkinter as ct
 import tkinter as ttk
+from tkinter import messagebox
 import PIL
+import mysql.connector as msql
+
 
 ct.set_appearance_mode("dark")  
 ct.set_default_color_theme("green")
 
-img = PIL.Image.open("Assets/png-login.png")
+img = PIL.Image.open("./Assets/png-login.png")
 
 class Login(ct.CTk):
     def __init__(self):
@@ -20,6 +23,10 @@ class Login(ct.CTk):
                         font=("Helvetica", 20))
         self.label1.pack(pady=20, padx=10)
         
+        self.label2 = ct.CTkLabel(self, text="",
+                        font=("Helvetica", 16))
+        self.label2.pack(pady=20, padx=10)
+        
         self.username = ct.CTkEntry(self.frame1, placeholder_text="enter username")
         self.username.pack(pady=10)
         
@@ -27,9 +34,34 @@ class Login(ct.CTk):
         self.password.pack(pady=10)
         
         self.button1 = ct.CTkButton(self.frame1, text="login", command=self.login_check, image=ct.CTkImage(dark_image=img, light_image=img))
-        self.button1.pack(pady=20, padx=120)    
+        self.button1.pack(pady=20, padx=120)
+            
     def login_check(self):
-        self.label1.configure(text="button pressed")
+        username = self.username.get()
+        password = self.password.get()
+        if not(username) or not(password):
+            self.label2.configure(text="please enter username and password")
+            messagebox.showerror("Error", "Type Username and Password")
+        else:
+            try:
+                mydb=msql.connect(host="localhost", user='root',
+                                password='QueueThatW@69',
+                                database='registration')
+                mycursor=mydb.cursor()
+                #messagebox.showerror("","Connected to database")
+                command = "use registration"
+                mycursor.execute(command)
+                command="select * from login where username=%s and password=%s"
+                mycursor.execute(command,(username,password))
+                
+                myresult=mycursor.fetchone()
+                if myresult != None:
+                    messagebox.showerror("","Login Successful")
+                else:
+                    messagebox.showerror("","Login Failed")
+            except:
+                messagebox.showerror("","Couldn't connect to database")
+            
 
 app = Login()
 app.mainloop()
